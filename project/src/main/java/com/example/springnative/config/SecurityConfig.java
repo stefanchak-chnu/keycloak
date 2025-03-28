4da531659938547237d93d9a90516cab557103bc
stefanchak-chnu <stefanchak.vitalii@chnu.edu.ua>
2025-03-28 17:11:47 +0200

    added roles

--- project/src/main/java/com/example/springnative/config/SecurityConfig.java
+++ project/src/main/java/com/example/springnative/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.springnative.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -10,18 +11,18 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/actuator/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
-                );
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+            );
         return http.build();
     }
 
--- project/src/main/java/com/example/springnative/controller/AuthorController.java
+++ project/src/main/java/com/example/springnative/controller/AuthorController.java
@@ -5,6 +5,8 @@ import com.example.springnative.payload.response.AuthorResponse;
 import com.example.springnative.service.AuthorService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,12 +23,19 @@ public class AuthorController {
     private final AuthorService authorService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public AuthorResponse create(@Valid @RequestBody AuthorRequest authorRequest) {
         return authorService.createAuthor(authorRequest);
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     public List<AuthorResponse> getAll() {
         return authorService.getAllAuthors();
+    }
+
+    @GetMapping("/debug")
+    public String debug(Authentication authentication) {
+        return authentication.getAuthorities().toString();
     }
 }
